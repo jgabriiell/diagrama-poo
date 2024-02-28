8dcdc49e30240dae216aed2fc119bab39ebb63d0
João Gabriel <jgabriiell33@gmail.com>
2024-02-28 11:57:32 -0300

    commiting classes

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,16 @@ public class Main {
 
         Iphone1 joao = new Iphone1("João");
 
+        joao.selecionarMusica();
         joao.tocarMusica();
+        joao.pausarMusica();
+
+        joao.ligar();
+        joao.atender();
+        joao.iniciarCorreioDeVoz();
+
+        joao.exibirPagina();
+        joao.exibirNovaPagina();
+        joao.atualizarAba();
     }
 }
